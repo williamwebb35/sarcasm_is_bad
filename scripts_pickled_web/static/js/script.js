20fa5696fc1f8bb4c6856e8afe825de7c93ad837
William C. Webb <williamwebb35@gmail.com>
2020-08-31 21:37:50 -0700

    adding static folder and files

---
+++ scripts_pickled_web/static/js/script.js
@@ -0,0 +1,11 @@
+// The following is based on https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
